584d5d6ecc30e21bba20bf4f32a3d2a3c6be75b2
Bradley Ellert <bellert@dwavesys.com>
2017-12-15 15:52:21 -0800

    Add C++ coverage scripts
    Base on qbsolv
    Remove lines for building gtest as this is done by cmake

---
+++ ci/travis/osx-cpp-coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -x
+
+# Install dependencies
+### pip install --user cpp-coveralls;
+export CC=$C_COMPILER;
+export CXX=$CXX_COMPILER;
+
+# Build the tests
+mkdir build;
+cd build;
+cmake .. -DMINORMINER_BUILD_TESTS=ON;
+make CC=$CC CXX=$CXX;
+
+# Run the tests
+./tests/all_tests;
+
+# Stop after running the unit tests, the linux builds will submit the coverage data
+#
+# Gather the test coverage files
+### find . \( -name '*.gcno' -or -name '*.gcda' \) -exec mv {} .. \;
+### cd -;
+
+# Submit the traces to coveralls
+### coveralls --exclude tests -E '.*gtest.*' --gcov-options '\-lp';
