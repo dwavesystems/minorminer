618245b076c4d89345a7afb18e777126cdc4c92d
Bradley Ellert <bellert@dwavesys.com>
2018-01-03 13:16:49 -0800

    Update version 0.1.1.dev3 -> 0.1.1.dev4

--- python/setup.py
+++ python/setup.py
@@ -47,7 +47,7 @@ setup(
     author="Kelly Boothby",
     author_email="boothby@dwavesys.com",
     url="https://github.com/dwavesystems/minorminer",
-    version="0.1.1.dev3",
+    version="0.1.1.dev4",
     license="Apache 2.0",
     ext_modules=cythonize(Extension(
         name="minorminer",
