f8f497e4027642a0992085f10886b6a3abddf7c4
Bradley Ellert <bellert@dwavesys.com>
2018-01-23 12:59:32 -0800

    Deploy sdist Fix old build scripts too

--- ci/travis/osx-build-wheel.sh
+++ ci/travis/osx-build-wheel.sh
@@ -3,8 +3,8 @@ set -e -x
 
 python --version
 pip install delocate
-pip install -r ./python/requirements.txt
-pip wheel -e ./python -w raw-wheelhouse/
+pip install -r cython>=0.27
+pip wheel -e ./ -w raw-wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in raw-wheelhouse/*.whl; do
