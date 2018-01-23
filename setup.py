555c9e513efe976aea581e0785826fd7dcd181ba
Bradley Ellert <bellert@dwavesys.com>
2018-01-23 12:19:34 -0800

    Find ALL *.hpp with manifest

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     author="Kelly Boothby",
     author_email="boothby@dwavesys.com",
     url="https://github.com/dwavesystems/minorminer",
-    version="0.1.2.dev4",
+    version="0.1.2.dev5",
     license="Apache 2.0",
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext_compiler_check}
