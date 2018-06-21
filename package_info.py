c1e67cdab5133ebc1e0c1b96af8be320f365e907
Bradley Ellert <bellert@dwavesys.com>
2018-06-21 08:42:50 -0700

    0.1.3 --> 0.1.4

--- package_info.py
+++ package_info.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Kelly Boothby"
 __authoremail__ = "boothby@dwavesys.com"
 __description__ = "heuristic algorithm to find graph minor embeddings"
