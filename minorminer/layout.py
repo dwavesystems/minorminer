282923f4b503cd8a397f7b6b06bf2ceaa495bc1a
Stefan Hannie <shannie@dwavesys.com>
2020-01-10 17:03:48 -0800

    Documentation
    Added more documentation and formatting

--- minorminer/layout.py
+++ minorminer/layout.py
@@ -8,14 +8,13 @@ from scipy.spatial.distance import euclidean
 
 def get_embedding(S, T, layout="kamada_kawai_2d", placement="closest", chains="singleton", **kwargs):
     """
-    Given a placement (a map from vertices of S to vertices of T), 
-    form chains that will serve as embedding hints for minerminor.
+    Tries to embed S in T by computing layout aware initial_chains and passing them to minorminer.
 
     Parameters
     ----------
-    S : networkx graph
+    S : NetworkX graph
         The graph you are embedding (source).
-    T : networkx graph
+    T : NetworkX graph
         The graph you are embedding into (target).
     layout : str (default "kamada_kawai_2d")
         A strategy to be used when calling get_layout().
@@ -29,8 +28,8 @@ def get_embedding(S, T, layout="kamada_kawai_2d", placement="closest", chains="s
     Returns
     -------
     emb : dict
-        Output is dependant upon kwargs passed to minonminer, but more or less
-        emb is a mapping from vertices of S (keys) to chains in T (values).
+        Output is dependant upon kwargs passed to minonminer, but more or less emb is a mapping from vertices of 
+        S (keys) to chains in T (values).
     """
     S_layout = get_layout(S, strategy=layout)
     T_layout = get_layout(T, strategy=layout)
@@ -88,10 +87,10 @@ def get_placement(S_layout, T_layout, strategy="closest"):
         A layout for T; i.e. a map from T to the plane..
     strategy : string (default "closest")
         Different placement strategies to use. Implemented strategies are:
-            - "closest" : For each vertex u in S_layout and each vertex v in T_layout, 
-                            map u to the v with minimum Euclidean distance (||u - v||_2).
-            - "injective" : This is the same as the "closest" strategy, except the
-                            map is injective. This runs in O(|S||T|log(|T|)) time.
+            - "closest" : For each vertex u in S_layout and each vertex v in T_layout, map u to the v with minimum 
+                            Euclidean distance (||u - v||_2).
+            - "injective" : This is the same as the "closest" strategy, except the map is injective. This runs in 
+                            O(|S||T|log(|T|)) time.
 
     Returns
     -------
@@ -109,10 +108,7 @@ def get_placement(S_layout, T_layout, strategy="closest"):
 
     if strategy == "injective":
         X = nx.Graph()
-        for u, u_pos in S_layout.items():
-            for v, v_pos in T_layout.items():
-                assert u != v, "H and G must be labeled with different alphabets."
-                X.add_edge(u, v, weight=euclidean(u_pos, v_pos))
+        for u, u_pos in S_layout.items(euclidean(u_pos, v_pos))
         M = nx.bipartite.minimum_weight_full_matching(X, S_layout.keys())
         placement = {v: M[v] for v in S_layout.keys()}
         return placement
@@ -123,33 +119,31 @@ def get_placement(S_layout, T_layout, strategy="closest"):
 
 def get_chains(S, T, placement, strategy="singleton"):
     """
-    Given a placement (a map from vertices of S to vertices of T), 
-    form chains that will serve as embedding hints for minerminor.
+    Given a placement (a map from vertices of S to vertices of T), form chains that will serve as embedding hints for 
+    minerminor.
 
     Parameters
     ----------
-    S : networkx graph
+    S : NetworkX graph
         The graph you are embedding (source).
-    T : networkx graph
+    T : NetworkX graph
         The graph you are embedding into (target).
     placement : dict
         A mapping from vertices of S (keys) to vertices of T (values).
     strategy : string (default "singleton")
-        Different chain creation strategies to use. For each of the following descriptions
-        we assume that vertex u of S is mapped to vertex v of T. Implemented strategies are:
+        Different chain creation strategies to use. For each of the following descriptions we assume that vertex u of S 
+        is mapped to vertex v of T. Implemented strategies are:
             - "singleton" : A chain is of the form [v] for u.
-            - "extend" : Heuristically (with minorminer) compute optimal paths between v and 
-                            the image of each neighbor of u. The chain for u is the union of
-                            v together with these open paths.
-            - "neighborhood" : A chain is of the form N_T(v) (closed neighborhood of v in T) 
-                                for u.
-            - "neighborhood_extension" : First the extend algorithm is run to get extended chains,
-                                            then the closed neighborhood of that chain is returned.
+            - "extend" : Heuristically (with minorminer) compute optimal paths between v and the image of each neighbor 
+                            of u. The chain for u is the union of v together with these open paths.
+            - "neighborhood" : A chain is of the form N_T(v) (closed neighborhood of v in T) for u.
+            - "neighborhood_extension" : First the extend algorithm is run to get extended chains, then the closed 
+                                            neighborhood of that chain is returned.
 
     Returns
     -------
     chains: dict
-        A mapping from vertices of H (keys) to chains of G (values).
+        A mapping from vertices of S (keys) to chains of T (values).
     """
     if strategy in ("neighborhood", "neighbourhood"):
         return {u: graph_utils.closed_neighbors(T, v) for u, v in placement.items()}
@@ -175,6 +169,24 @@ def get_chains(S, T, placement, strategy="singleton"):
 
 
 def extend_chains(S, T, initial_chains):
+    """
+    Extend initial_chains in T so that their structure matches that of S. That is, form an overlap embedding of S in T
+    where the initial_chains are subsets of the overlap embedding chains. This is done via minorminer.
+
+    Parameters
+    ----------
+    S : NetworkX graph
+        The graph you are embedding (source).
+    T : NetworkX graph
+        The graph you are embedding into (target).
+    initial_chains : dict
+        A mapping from vertices of S (keys) to chains of T (values).
+
+    Returns
+    -------
+    extended_chains: dict
+        A mapping from vertices of S (keys) to chains of T (values).
+    """
     # Extend the chains to minimal overlapped embedding
     miner = mm.miner(S, T, initial_chains=initial_chains)
     extended_chains = defaultdict(set)
@@ -194,8 +206,25 @@ def extend_chains(S, T, initial_chains):
     return extended_chains
 
 
-def closed_neighbors(H, u, second=False):
-    neighbors = set(v for v in H.neighbors(u))
+def closed_neighbors(G, u, second=False):
+    """
+    Returns the closed neighborhood of u in G.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        The graph you are considering.
+    u : NetworkX node
+        The node you are computing the closed neighborhood of.
+    second : bool (default False)
+        If True, compute the closed 2nd neighborhood.
+
+    Returns
+    -------
+    neighbors: set
+        A set of vertices of G.
+    """
+    neighbors = set(v for v in G.neighbors(u))
     if second:
-        return set((u,)) | neighbors | set(w for v in neighbors for w in H.neighbors(v))
+        return set((u,)) | neighbors | set(w for v in neighbors for w in G.neighbors(v))
     return set((u,)) | neighbors
