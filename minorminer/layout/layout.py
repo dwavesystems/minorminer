8ab8f5763ea877142078be5b74b5c6a25d3ebc60
Stefan Hannie <shannie@dwavesys.com>
2020-01-22 17:42:31 -0800

    Added Chimera layout
    The Chimera layout --> [-1,1]^2 was added to standardize layouts.

--- minorminer/layout/layout.py
+++ minorminer/layout/layout.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import numpy as np
 
+import dwave_networkx as dnx
+
 
 def kamada_kawai(G, d=2, **kwargs):
     """
-    The d-dimensional Kamada-Kawai spring layout. Axes are [0, 1] for all components except for the second component--it
-    is [0, -1]. This adhears to the dwave_networkx.draw_chimera() convention.
+    The d-dimensional Kamada-Kawai spring layout.
 
     Parameters
     ----------
@@ -19,7 +20,7 @@ def kamada_kawai(G, d=2, **kwargs):
     Returns
     -------
     layout : dict
-        A mapping from vertices of G (keys) to points in R^d (values).
+        A mapping from vertices of G (keys) to points in [-1, 1]^d (values).
     """
     # NetworkX has a bug #3658.
     # Once fixed, these can collapse and `dim=n` can be part of `**kwargs`.
@@ -36,17 +37,31 @@ def kamada_kawai(G, d=2, **kwargs):
         # The random_layout is in [0, 1]^d
         random_layout = nx.random_layout(G, dim=d, **random_kwargs)
         # Convert it to [-1, 1]^d
-        std_random_layout = standardize_coordinates(random_layout, "random")
+        std_random_layout = {v: list(map(lambda x: 2*x - 1, p))
+                             for v, p in random_layout.items()}
 
         return nx.kamada_kawai_layout(
             G, pos=std_random_layout, dim=d, **kwargs)
 
 
-def standardize_coordinates(layout, layout_type):
+def chimera(G, d=2, **kwargs):
     """
-    Most of networkx layouts return points in [-1, 1]^d. This is a helper function to standardize to this convention.
+    The d-dimensional Chimera layout adjusted so that it fills [-1, 1]^2 instead of [0, 1] x [0, -1]. As per the 
+    implementation of dnx.chimera_layout() coordinates beyond the second, in layouts with d > 2, are 0.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        A Chimera graph you want to compute the layout for.
+    d : int (default 2)
+        The dimension of the layout.
+    kwargs : dict
+        Keyword arguments are passed to dnx.chimera_layout().
+
+    Returns
+    -------
+    layout : dict
+        A mapping from vertices of G (keys) to points in [-1, 1]^d (values).
     """
-    if layout_type == "random":
-        return {v: list(map(lambda x: 2*x - 1, p)) for v, p in layout.items()}
-    else:
-        raise TypeError(f"The layout_type {layout_type} is not supported.")
+    return dnx.chimera_layout(
+        G, dim=d, center=(-1, 1), scale=2, **kwargs)
--- minorminer/layout/tests/test_layout.py
+++ minorminer/layout/tests/test_layout.py
@@ -1,6 +1,7 @@
 import random
 import unittest
 
+import dwave_networkx as dnx
 import networkx as nx
 from numpy import array
 
@@ -32,110 +33,16 @@ class TestLayout(unittest.TestCase):
                     self.assertGreaterEqual(coordinate, -1)
                     self.assertLessEqual(coordinate, 1)
 
-    def test_kamada_kawai_1(self):
+    def test_chimera(self):
         """
-        Tests that the Kamada-Kawai layout is correct for K_4,4 in 1d.
+        Tests that the layout is correct for Chimera(4); i.e. that it matches the pre-computed values in [-1, 1]^2.
         """
-        G = nx.complete_bipartite_graph(4, 4)
+        G = dnx.chimera_graph(4)
+        my_chimera = layout.chimera(G)
 
-        my_kamada_kawai = layout.kamada_kawai(G, d=1)
-        kamada_kawai = {
-            0: array([-1.]),
-            1: array([-0.74999997]),
-            2: array([-0.49999993]),
-            3: array([-0.2499999]),
-            4: array([0.2499999]),
-            5: array([0.49999993]),
-            6: array([0.74999997]),
-            7: array([1.])
-        }
-
-        for v, p in kamada_kawai.items():
+        for v, p in pre_computed_chimera.items():
             for i, x in enumerate(p):
-                coordinate = my_kamada_kawai[v][i]
-                self.assertAlmostEqual(coordinate, x)
-                self.assertGreaterEqual(coordinate, -1)
-                self.assertLessEqual(coordinate, 1)
-
-    def test_kamada_kawai_2(self):
-        """
-        Tests that the Kamada-Kawai layout is correct for K_4,4 in 2d.
-        """
-        G = nx.complete_bipartite_graph(4, 4)
-
-        my_kamada_kawai = layout.kamada_kawai(G)
-        kamada_kawai = {
-            0: array([1., -0.06320495]),
-            1: array([0.53618338, 0.48755357]),
-            2: array([-0.03438645,  0.72389135]),
-            3: array([-0.75179941,  0.66241415]),
-            4: array([-0.99999975,  0.06320475]),
-            5: array([-0.53618328, -0.48755351]),
-            6: array([0.03438642, -0.72389123]),
-            7: array([0.75179908, -0.66241412])
-        }
-
-        for v, p in kamada_kawai.items():
-            for i, x in enumerate(p):
-                coordinate = my_kamada_kawai[v][i]
-                self.assertAlmostEqual(coordinate, x)
-                self.assertGreaterEqual(coordinate, -1)
-                self.assertLessEqual(coordinate, 1)
-
-    def test_kamada_kawai_3(self):
-        """
-        Tests that the Kamada-Kawai layout is correct for K_4,4 in 3d.
-        """
-        G = nx.complete_bipartite_graph(4, 4)
-
-        my_kamada_kawai = layout.kamada_kawai(G, d=3, seed=seed)
-        kamada_kawai = {
-            0: array([-0.98709905, -0.1467455, -0.14392173]),
-            1: array([0.64128769, -0.68976331, -0.36001532]),
-            2: array([0.98708649, 0.14677012, 0.14393496]),
-            3: array([-0.64126773,  0.68975957,  0.3600116]),
-            4: array([-0.12808806,  0.01386342, -0.99999759]),
-            5: array([-0.21772691, -0.85033526,  0.49607745]),
-            6: array([0.2177274,  0.85035448, -0.49608937]),
-            7: array([0.12808017, -0.01390352,  1.])
-        }
-
-        for v, p in kamada_kawai.items():
-            for i, x in enumerate(p):
-                coordinate = my_kamada_kawai[v][i]
-                self.assertAlmostEqual(coordinate, x)
-                self.assertGreaterEqual(coordinate, -1)
-                self.assertLessEqual(coordinate, 1)
-
-    def test_kamada_kawai_10(self):
-        """
-        Tests that the Kamada-Kawai layout is correct for K_4,4 in 10d.
-        """
-        G = nx.complete_bipartite_graph(4, 4)
-
-        my_kamada_kawai = layout.kamada_kawai(G, d=10, seed=seed)
-        kamada_kawai = {
-            0: array([-1.,  0.14029899,  0.28687387,  0.18651374,  0.39668944,
-                      -0.43120035,  0.15087843,  0.15002928,  0.70819066, -0.70950176]),
-            1: array([0.74702881,  0.77938909,  0.09432316, -0.50056582, -0.24865196,
-                      -0.25723954,  0.60890604, -0.69002551,  0.13703349,  0.33409856]),
-            2: array([-0.22853146, -0.45075593, -0.7181411,  0.10456035,  0.60312017,
-                      0.89164998, -0.11406198, -0.36983147, -0.62121529,  0.23176585]),
-            3: array([0.48150304, -0.46892587,  0.33694644,  0.20949211, -0.75115823,
-                      -0.20321651, -0.64572577,  0.90983074, -0.2240106,  0.1436377]),
-            4: array([0.5653143, -0.13324933, -0.14883313,  0.96652688,  0.13539118,
-                      0.35343568,  0.85628352,  0.38361186,  0.45911682, -0.04684403]),
-            5: array([-0.25021981, -0.14126589, -0.79179585, -0.66333981, -0.9165724,
-                      0.12704637, -0.19481089, -0.20852614,  0.21568323, -0.62970882]),
-            6: array([-0.26440385,  0.78508537,  0.46821357,  0.64575101,  0.02804133,
-                      -0.06709947, -0.84028861, -0.50792323, -0.43359484,  0.22454071]),
-            7: array([-0.05069104, -0.51057644,  0.47241304, -0.94893846,  0.75314046,
-                      -0.41337617,  0.17881927,  0.33283447, -0.24120348,  0.45201179])
-        }
-
-        for v, p in kamada_kawai.items():
-            for i, x in enumerate(p):
-                coordinate = my_kamada_kawai[v][i]
+                coordinate = my_chimera[v][i]
                 self.assertAlmostEqual(coordinate, x)
                 self.assertGreaterEqual(coordinate, -1)
                 self.assertLessEqual(coordinate, 1)
@@ -190,6 +97,137 @@ pre_computed_kamada_kawai = {
         7: array([-0.05069104, -0.51057644,  0.47241304, -0.94893846,  0.75314046,
                   -0.41337617,  0.17881927,  0.33283447, -0.24120348,  0.45201179])
     }
+}
+
+pre_computed_chimera = {
+    0: array([-1.,  0.84]),
+    4: array([-0.84,  1.]),
+    5: array([-0.84,  0.92]),
+    6: array([-0.84,  0.76]),
+    7: array([-0.84,  0.68]),
+    1: array([-0.92,  0.84]),
+    2: array([-0.76,  0.84]),
+    3: array([-0.68,  0.84]),
+    32: array([-1.,  0.28]),
+    36: array([-0.84,  0.44]),
+    37: array([-0.84,  0.36]),
+    38: array([-0.84,  0.2]),
+    39: array([-0.84,  0.12]),
+    33: array([-0.92,  0.28]),
+    34: array([-0.76,  0.28]),
+    35: array([-0.68,  0.28]),
+    64: array([-1., -0.28]),
+    68: array([-0.84, -0.12]),
+    69: array([-0.84, -0.2]),
+    70: array([-0.84, -0.36]),
+    71: array([-0.84, -0.44]),
+    65: array([-0.92, -0.28]),
+    66: array([-0.76, -0.28]),
+    67: array([-0.68, -0.28]),
+    96: array([-1., -0.84]),
+    100: array([-0.84, -0.68]),
+    101: array([-0.84, -0.76]),
+    102: array([-0.84, -0.92]),
+    103: array([-0.84, -1.]),
+    97: array([-0.92, -0.84]),
+    98: array([-0.76, -0.84]),
+    99: array([-0.68, -0.84]),
+    8: array([-0.44,  0.84]),
+    12: array([-0.28,  1.]),
+    13: array([-0.28,  0.92]),
+    14: array([-0.28,  0.76]),
+    15: array([-0.28,  0.68]),
+    9: array([-0.36,  0.84]),
+    10: array([-0.2,  0.84]),
+    11: array([-0.12,  0.84]),
+    40: array([-0.44,  0.28]),
+    44: array([-0.28,  0.44]),
+    45: array([-0.28,  0.36]),
+    46: array([-0.28,  0.2]),
+    47: array([-0.28,  0.12]),
+    41: array([-0.36,  0.28]),
+    42: array([-0.2,  0.28]),
+    43: array([-0.12,  0.28]),
+    72: array([-0.44, -0.28]),
+    76: array([-0.28, -0.12]),
+    77: array([-0.28, -0.2]),
+    78: array([-0.28, -0.36]),
+    79: array([-0.28, -0.44]),
+    73: array([-0.36, -0.28]),
+    74: array([-0.2, -0.28]),
+    75: array([-0.12, -0.28]),
+    104: array([-0.44, -0.84]),
+    108: array([-0.28, -0.68]),
+    109: array([-0.28, -0.76]),
+    110: array([-0.28, -0.92]),
+    111: array([-0.28, -1.]),
+    105: array([-0.36, -0.84]),
+    106: array([-0.2, -0.84]),
+    107: array([-0.12, -0.84]),
+    16: array([0.12, 0.84]),
+    20: array([0.28, 1.]),
+    21: array([0.28, 0.92]),
+    22: array([0.28, 0.76]),
+    23: array([0.28, 0.68]),
+    17: array([0.2, 0.84]),
+    18: array([0.36, 0.84]),
+    19: array([0.44, 0.84]),
+    48: array([0.12, 0.28]),
+    52: array([0.28, 0.44]),
+    53: array([0.28, 0.36]),
+    54: array([0.28, 0.2]),
+    55: array([0.28, 0.12]),
+    49: array([0.2, 0.28]),
+    50: array([0.36, 0.28]),
+    51: array([0.44, 0.28]),
+    80: array([0.12, -0.28]),
+    84: array([0.28, -0.12]),
+    85: array([0.28, -0.2]),
+    86: array([0.28, -0.36]),
+    87: array([0.28, -0.44]),
+    81: array([0.2, -0.28]),
+    82: array([0.36, -0.28]),
+    83: array([0.44, -0.28]),
+    112: array([0.12, -0.84]),
+    116: array([0.28, -0.68]),
+    117: array([0.28, -0.76]),
+    118: array([0.28, -0.92]),
+    119: array([0.28, -1.]),
+    113: array([0.2, -0.84]),
+    114: array([0.36, -0.84]),
+    115: array([0.44, -0.84]),
+    24: array([0.68, 0.84]),
+    28: array([0.84, 1.]),
+    29: array([0.84, 0.92]),
+    30: array([0.84, 0.76]),
+    31: array([0.84, 0.68]),
+    25: array([0.76, 0.84]),
+    26: array([0.92, 0.84]),
+    27: array([1., 0.84]),
+    56: array([0.68, 0.28]),
+    60: array([0.84, 0.44]),
+    61: array([0.84, 0.36]),
+    62: array([0.84, 0.2]),
+    63: array([0.84, 0.12]),
+    57: array([0.76, 0.28]),
+    58: array([0.92, 0.28]),
+    59: array([1., 0.28]),
+    88: array([0.68, -0.28]),
+    92: array([0.84, -0.12]),
+    93: array([0.84, -0.2]),
+    94: array([0.84, -0.36]),
+    95: array([0.84, -0.44]),
+    89: array([0.76, -0.28]),
+    90: array([0.92, -0.28]),
+    91: array([1., -0.28]),
+    120: array([0.68, -0.84]),
+    124: array([0.84, -0.68]),
+    125: array([0.84, -0.76]),
+    126: array([0.84, -0.92]),
+    127: array([0.84, -1.]),
+    121: array([0.76, -0.84]),
+    122: array([0.92, -0.84]),
+    123: array([1., -0.84])
 }
 
 if __name__ == '__main__':
