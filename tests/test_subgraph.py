056b0b2988ccb305605df94ce3c235868c410ce2
Kelly Boothby <boothby@dwavesys.com>
2023-01-16 11:43:53 -0800

    updated test to be solvable

--- tests/test_subgraph.py
+++ tests/test_subgraph.py
@@ -5,7 +5,7 @@ import unittest, random, itertools, dwave_networkx as dnx, networkx as nx, os
 
 class TestSubgraph(unittest.TestCase):
     def test_smoketest(self):
-        b = nx.complete_bipartite_graph(3, 3)
+        b = nx.complete_bipartite_graph(4, 4)
         c = nx.cubical_graph()
         emb = subgraph.find_subgraph(c, b)
         emb = {k: [v] for k, v in emb.items()}
