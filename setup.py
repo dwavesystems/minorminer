ffdce7dc91044f91389c07770d7abfa4de9f64a0
Alexander Condello <arcondello@gmail.com>
2023-08-30 12:24:20 -0700

    Drop Python 3.7 support

--- setup.py
+++ setup.py
@@ -141,14 +141,13 @@ classifiers = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
 ]
 
-python_requires = '>=3.7'
+python_requires = '>=3.8'
 install_requires = [
     "dwave-networkx>=0.8.10",
     "fasteners>=0.15",
