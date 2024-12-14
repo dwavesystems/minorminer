be6b988d0a4d59771253620917d61cdb73713fd3
Andy Zhang <azhang@dwavesys.com>
2024-12-13 16:16:47 -0800

    Minor fix to imports

--- minorminer/utils/parallel_embeddings.py
+++ minorminer/utils/parallel_embeddings.py
@@ -18,7 +18,6 @@ onto a target graph or provide supporting functionality.
 import warnings
 
 import dwave_networkx as dnx
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from typing import Union
